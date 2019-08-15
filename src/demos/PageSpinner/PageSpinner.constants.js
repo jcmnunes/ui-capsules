f7f866eda4302717f34428d3e81e6fe98fd50029
Jose Nunes <josenunes34@gmail.com>
2019-08-15 23:32:34 +0100

    feat(component): PageSpinner
    UIC-21

---
+++ src/demos/PageSpinner/PageSpinner.constants.js
@@ -0,0 +1,12 @@
+export const GITHUB_URL =
+  'https://github.com/jcmnunes/ui-capsules/blob/master/src/lib/Spinner/Spinner.jsx';
+
+export const PROPS = [
+  {
+    name: 'children',
+    isRequired: false,
+    type: 'node',
+    defaultValue: 'null',
+    description: 'The text or element to render above the spinner.',
+  },
+];
