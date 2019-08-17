520a25d88fc1a93d36ebff6c3c0633589ae832da
Jose Nunes <josenunes34@gmail.com>
2019-08-17 19:07:54 +0100

    feat(component): Icon20
    UIC-14

---
+++ src/components/IconGrid/IconGrid.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const IconGrid = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export default IconGrid;
--- src/demos/Icon20/Icon20.demo.jsx
+++ src/demos/Icon20/Icon20.demo.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import Select from '../../lib/Select/Select';
 import Demo, { StyledSelect } from '../../components/Demo/Demo';
 import Icon20 from '../../lib/Icon20/Icon20';
-import { GITHUB_URL, ICON_OPTS } from './Icon20.constants';
+import { GITHUB_URL } from './Icon20.constants';
+import { ICONS } from '../../lib/Icon20/Icon20.constants';
 
 const Icon20Demo = () => {
-  const [icon, setIcon] = useState(ICON_OPTS[0]);
-
+  const iconOpts = useMemo(() => Object.keys(ICONS).map(icon => ({ value: icon, label: icon })), [
+    ICONS,
+  ]);
+  const [icon, setIcon] = useState(iconOpts[0]);
   const Component = <Icon20 icon={icon.value} />;
 
   return (
@@ -14,7 +17,7 @@ const Icon20Demo = () => {
       <>
         <StyledSelect>
           <pre>icon</pre>
-          <Select value={icon} options={ICON_OPTS} onChange={opt => setIcon(opt)} />
+          <Select value={icon} options={iconOpts} onChange={opt => setIcon(opt)} />
         </StyledSelect>
       </>
     </Demo>
