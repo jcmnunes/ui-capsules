b6969a1c5cc07d8ecaae6311286f1a4de6b840cf
Jose Nunes <josenunes34@gmail.com>
2019-08-17 19:07:54 +0100

    feat(component): Icon20
    UIC-14

--- src/demos/Icon20/Icon20.view.jsx
+++ src/demos/Icon20/Icon20.view.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Demo from './Icon20.demo';
+import { ICONS } from '../../lib/Icon20/Icon20.constants';
 import Usage from '../../components/Usage/Usage';
 import { Paragraph, SubTitle, Title, UL } from '../../components/common/Typography';
 import Wrapper from '../../components/common/Wrapper';
 import PropsTable from '../../components/PropsTable/PropsTable';
+import IconGrid from '../../components/IconGrid/IconGrid';
 import { ICON20_PROPS } from './Icon20.constants';
+import Icon20 from '../../lib/Icon20/Icon20';
+import IconCard from '../../components/IconCard/IconCard';
 
 const ButtonDemo = () => {
   return (
@@ -21,13 +25,23 @@ const ButtonDemo = () => {
       <Paragraph>
         The icon is especially useful to render inside buttons (as the iconBefore and iconAfter
         props). It can also be used to render actions inline and when there is not much space
-        available).
+        available.
       </Paragraph>
       <SubTitle>Usage</SubTitle>
       <Usage>{"import { Icon20 } from '@binarycapsule/ui-capsules';"}</Usage>
       <Demo />
       <SubTitle>Props</SubTitle>
       <PropsTable props={ICON20_PROPS} />
+      <SubTitle id="icons">Icons</SubTitle>
+      <IconGrid>
+        {Object.keys(ICONS)
+          .sort()
+          .map((icon, index) => (
+            <IconCard label={icon} isLastInRow={(index + 1) % 5 === 0}>
+              <Icon20 icon={icon} />
+            </IconCard>
+          ))}
+      </IconGrid>
     </Wrapper>
   );
 };
