6ddc62f98d50b757bd5910fb62173ca5a300b6ff
Jose Nunes <josenunes34@gmail.com>
2019-08-17 19:30:39 +0100

    chore: lint app files too
    Other changes:
    - Fix linter errors
    
    UIC-14

--- src/demos/Icon20/Icon20.constants.js
+++ src/demos/Icon20/Icon20.constants.js
@@ -1,14 +1,6 @@
 export const GITHUB_URL =
   'https://github.com/jcmnunes/ui-capsules/blob/master/src/lib/Icon20/Icon20.jsx';
 
-export const ICON_OPTS = [
-  { value: 'AREA_GRAPH', label: 'AREA_GRAPH' },
-  { value: 'BAR_GRAPH', label: 'BAR_GRAPH' },
-  { value: 'PLUS', label: 'PLUS' },
-  { value: 'CODE', label: 'CODE' },
-  { value: 'CLOSE', label: 'CLOSE' },
-];
-
 export const ICON20_PROPS = [
   {
     name: 'icon',
