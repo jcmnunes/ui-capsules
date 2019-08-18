528f7bde01e8f39c858cd8ac38c54eaaf750f373
Jose Nunes <josenunes34@gmail.com>
2019-08-18 23:58:26 +0100

    fix: Remove import from sc macro

--- src/lib/Message/Message.jsx
+++ src/lib/Message/Message.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { keyframes } from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components';
 import Glyph from '../Glyph/Glyph';
 import theme from '../theme';
 
