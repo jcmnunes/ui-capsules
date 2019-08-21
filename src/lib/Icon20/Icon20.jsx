05af67f1c57c41217ec16afbbea712e081fb9438
Jose Nunes <josenunes34@gmail.com>
2019-08-22 00:56:40 +0100

    feat: add sizes to icons
    UIC-35

--- src/lib/Icon20/Icon20.jsx
+++ src/lib/Icon20/Icon20.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ICONS } from './Icon20.constants';
 
-const Icon20 = ({ icon }) => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 20 20">
-      <path fill="currentColor" d={ICONS[icon]} />
-    </svg>
-  );
+const sizes = {
+  small: '16px',
+  medium: '20px',
+  large: '24px',
 };
+
+const Icon20 = ({ icon, size }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={sizes[size]} viewBox="0 0 20 20">
+    <path fill="currentColor" d={ICONS[icon]} />
+  </svg>
+);
 Icon20.displayName = 'Icon20';
 
+Icon20.defaultProps = {
+  size: 'medium',
+};
+
 Icon20.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
   icon: PropTypes.string.isRequired,
 };
 
