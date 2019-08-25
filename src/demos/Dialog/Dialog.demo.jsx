c6fad04e0c57a05ac3de19e9497a7d0ab6d6d5d9
Jose Nunes <josenunes34@gmail.com>
2019-08-26 00:54:21 +0100

    feat(Component): new component - Dialog
    Other changes:
    - New modal animation
    
    UIC-20

---
+++ src/demos/Dialog/Dialog.demo.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import Button from '../../lib/Button/Button';
+import Input from '../../lib/Input/Input';
+import Demo, { Booleans, StyledCheckbox, StyledSelect } from '../../components/Demo/Demo';
+import { GITHUB_URL } from './Dialog.constants';
+import Dialog from '../../lib/Dialog/Dialog';
+
+const DialogDemo = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [children, setChildren] = useState('Are you sure you want to delete?');
+  const [contentLabel, setContentLabel] = useState('Example dialog');
+
+  const Component = (
+    <Button appearance="primary" iconBefore="APP_WINDOW" onClick={() => setIsOpen(true)}>
+      Open Dialog
+    </Button>
+  );
+
+  const code = `<Dialog
+  isOpen={isOpen}
+  onRequestClose={() => setIsOpen(false)}
+  contentLabel="${contentLabel}"
+  actions={[
+    {
+      name: 'Cancel',
+      appearance: 'secondary',
+      action: () => setIsOpen(false),
+    },
+    {
+      name: 'Delete',
+      appearance: 'error',
+      action: () => alert('Delete action called'),
+    },
+  ]}
+>
+  ${children}
+</Dialog>
+`;
+
+  return (
+    <Demo codeURL={GITHUB_URL} component={Component} code={code}>
+      <>
+        <Booleans>
+          <StyledCheckbox checked={isOpen} onChange={() => setIsOpen(!isOpen)} appearance="success">
+            <pre>isOpen</pre>
+          </StyledCheckbox>
+        </Booleans>
+        <StyledSelect>
+          <pre>children</pre>
+          <Input type="text" value={children} onChange={ev => setChildren(ev.target.value)} />
+        </StyledSelect>
+        <StyledSelect>
+          <pre>contentLabel</pre>
+          <Input
+            type="text"
+            value={contentLabel}
+            onChange={ev => setContentLabel(ev.target.value)}
+          />
+        </StyledSelect>
+      </>
+
+      <Dialog
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        contentLabel="Example Modal"
+        actions={[
+          {
+            name: 'Cancel',
+            appearance: 'secondary',
+            action: () => setIsOpen(false),
+          },
+          {
+            name: 'Delete',
+            appearance: 'error',
+            // eslint-disable-next-line no-alert
+            action: () => alert('Delete action called'),
+          },
+        ]}
+      >
+        {children}
+      </Dialog>
+    </Demo>
+  );
+};
+
+export default DialogDemo;
---
+++ src/demos/Dialog/Dialog.view.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Demo from './Dialog.demo';
+import Usage from '../../components/Usage/Usage';
+import { Paragraph, SubSubTitle, SubTitle, Title } from '../../components/common/Typography';
+import Wrapper from '../../components/common/Wrapper';
+import PropsTable from '../../components/PropsTable/PropsTable';
+import Anchor from '../../lib/Anchor/Anchor';
+import { exampleAppElement, PROPS } from './Dialog.constants';
+
+const DialogDemo = () => {
+  return (
+    <Wrapper>
+      <Title>Dialog</Title>
+      <Paragraph>
+        Accessible dialog based on{' '}
+        <Anchor href="http://reactcommunity.org/react-modal">react-modal</Anchor>.
+      </Paragraph>
+      <SubTitle>Usage</SubTitle>
+      <Usage>{"import { Dialog } from '@binarycapsule/ui-capsules';"}</Usage>
+      <Paragraph>
+        To improve A11y, at the entry point of the app call the <code>setAppElement</code> method
+        with a query selector identifying the root of your app.
+      </Paragraph>
+      <Usage>{exampleAppElement}</Usage>
+      <Paragraph>
+        See{' '}
+        <Anchor href="http://reactcommunity.org/react-modal/accessibility/#app-element">
+          react-modal docs
+        </Anchor>{' '}
+        for more information.
+      </Paragraph>
+      <Demo />
+      <SubTitle>Props</SubTitle>
+      <PropsTable props={PROPS} />
+      <SubSubTitle>Note:</SubSubTitle>
+      <Paragraph>
+        This component accepts all props from the{' '}
+        <Anchor href="http://reactcommunity.org/react-modal">react-modal API</Anchor>. Please refer
+        to the <Anchor href="http://reactcommunity.org/react-modal/#usage">docs</Anchor> for more
+        information.
+      </Paragraph>
+    </Wrapper>
+  );
+};
+
+export default DialogDemo;
