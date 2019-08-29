67cbac5e25971679a2dd36d44f7c042daa03e268
Jose Nunes <josenunes34@gmail.com>
2019-08-30 00:18:45 +0100

    feat(Components): Anchor component
    Other changes:
    - Add the redirects file
    - Remove animation from modal
    - New badge ➜ Netlify
    
    UIC-44

---
+++ src/demos/Anchor/Anchor.demo.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Demo, { StyledSelect } from '../../components/Demo/Demo';
+import Anchor from '../../lib/Anchor/Anchor';
+import Input from '../../lib/Input/Input';
+import { GITHUB_URL, HREF_OPTS } from './Anchor.constants';
+import Select from '../../lib/Select/Select';
+
+const AnchorDemo = () => {
+  const [href, setHref] = useState(HREF_OPTS[0]);
+  const [children, setChildren] = useState('Go to example website');
+
+  const Component = <Anchor href={href.value}>{children}</Anchor>;
+
+  return (
+    <Demo codeURL={GITHUB_URL} component={Component}>
+      <>
+        <StyledSelect>
+          <pre>href</pre>
+          <Select value={href} options={HREF_OPTS} onChange={opt => setHref(opt)} />
+        </StyledSelect>
+        <StyledSelect>
+          <pre>children</pre>
+          <Input type="text" value={children} onChange={ev => setChildren(ev.target.value)} />
+        </StyledSelect>
+      </>
+    </Demo>
+  );
+};
+
+export default AnchorDemo;
