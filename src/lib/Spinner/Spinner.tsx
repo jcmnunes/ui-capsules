d8dce2df4a2b854862a5c2d7cd20413054a6ac51
Jose Nunes <josenunes34@gmail.com>
2020-05-29 15:27:18 +0100

    fix(Spinner): size should be optional

--- src/lib/Spinner/Spinner.tsx
+++ src/lib/Spinner/Spinner.tsx
@@ -22,16 +22,22 @@ const spin = keyframes`
 `;
 
 interface SpinnerProps {
-  size: ElementSize;
+  size?: ElementSize;
   appearance?: 'light' | 'dark';
 }
 export const Spinner = styled.span<SpinnerProps>`
   display: inline-block;
   box-sizing: border-box;
   width: ${({ size }) =>
-    (size === 'small' && '14px') || (size === 'medium' && '18px') || (size === 'large' && '24px')};
+    (size === 'small' && '14px') ||
+    (size === 'medium' && '18px') ||
+    (size === 'large' && '24px') ||
+    '18px'};
   height: ${({ size }) =>
-    (size === 'small' && '14px') || (size === 'medium' && '18px') || (size === 'large' && '24px')};
+    (size === 'small' && '14px') ||
+    (size === 'medium' && '18px') ||
+    (size === 'large' && '24px') ||
+    '18px'};
   border-radius: 100%;
   border: ${props => `3px solid ${getColors(props).border}`};
   border-top-color: ${props => getColors(props).accent};
