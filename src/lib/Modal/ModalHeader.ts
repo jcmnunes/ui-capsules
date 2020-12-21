1d4163c998c0f5fc07fa345d79b0c1ade2e284eb
Jose Nunes <josenunes34@gmail.com>
2020-12-21 12:30:37 +0000

    fix: checkboxes and radios sizes

--- src/lib/Modal/ModalHeader.ts
+++ src/lib/Modal/ModalHeader.ts
@@ -5,7 +5,7 @@ export const ModalHeader = styled.div(({ theme }) => ({
   flexDirection: 'row',
   padding: '24px 64px 16px 32px',
   color: theme.colors.neutral['700'],
-  fontSize: '22px',
+  fontSize: theme.fontSizes.h5,
   fontWeight: 500,
   lineHeight: '32px',
 }));
