9cda4de17316e04386a3d026de4840f2c53d8051
Jose Nunes <josenunes34@gmail.com>
2020-12-21 11:38:33 +0000

    fix: IconButton forwardRef

--- src/lib/IconButton/IconButton.tsx
+++ src/lib/IconButton/IconButton.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { ButtonSize, ButtonVariant } from '../Button/Button.styles';
 import { MarginProps } from '../styledProps';
 import { StyledIconButton } from './IconButton.styles';
 import { IconVariant, VariantColor } from '../types';
 import { solidIcons } from '../Icon/Icon.constants';
 
-export interface Props extends React.ComponentPropsWithoutRef<'button'>, MarginProps {
+export interface IconButtonProps extends React.ComponentPropsWithoutRef<'button'>, MarginProps {
   icon: keyof typeof solidIcons;
   as?: 'button' | 'a';
   variant?: ButtonVariant;
@@ -19,30 +19,36 @@ const anchorProps = {
   rel: 'noreferrer noopener',
 };
 
-export const IconButton: React.FC<Props> = ({
-  icon,
-  as = 'button',
-  variant = 'solid',
-  variantColor = 'primary',
-  size = 'medium',
-  iconVariant = 'solid',
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  children,
-  ...rest
-}) => {
-  return (
-    <StyledIconButton
-      as={as}
-      variant={variant}
-      variantColor={variantColor}
-      size={size}
-      leftIcon={icon}
-      iconVariant={iconVariant}
-      {...(as === 'a' ? anchorProps : {})}
-      {...rest}
-    />
-  );
-};
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  (
+    {
+      icon,
+      as = 'button',
+      variant = 'solid',
+      variantColor = 'primary',
+      size = 'medium',
+      iconVariant = 'solid',
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      children,
+      ...rest
+    },
+    ref,
+  ) => {
+    return (
+      <StyledIconButton
+        ref={ref}
+        as={as}
+        variant={variant}
+        variantColor={variantColor}
+        size={size}
+        leftIcon={icon}
+        iconVariant={iconVariant}
+        {...(as === 'a' ? anchorProps : {})}
+        {...rest}
+      />
+    );
+  },
+);
 
 IconButton.displayName = 'IconButton';
 IconButton.defaultProps = {
