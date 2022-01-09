03a005505bd535516ff6bb840baa1245aef9d712
Jose Nunes <josenunes@hey.com>
2022-01-09 11:00:04 +0000

    feat: theme breakpoints

--- src/lib/stitches.config.ts
+++ src/lib/stitches.config.ts
@@ -92,7 +92,11 @@ export const { styled, css, keyframes, config, createTheme } = createStitches({
   },
 
   media: {
-    bp1: '(min-width: 480px)',
+    sm: '(min-width: 640px)',
+    md: '(min-width: 768px)',
+    lg: '(min-width: 1024px)',
+    xl: '(min-width: 1280px)',
+    '2xl': '(min-width: 1536px)',
   },
 
   utils: {
