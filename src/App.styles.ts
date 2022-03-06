166e10c61bfc5a33792e450b7bc67b45d50cdd63
Jose Nunes <josenunes@hey.com>
2022-03-06 15:43:30 +0000

    docs: add sidebar overlay

---
+++ src/App.styles.ts
@@ -0,0 +1,64 @@
+import { styled } from './lib';
+
+export const SidebarWrapper = styled('div', {
+  position: 'fixed',
+  flex: 2,
+  background: '$neutral100',
+  border: `1px solid $neutral200`,
+  display: 'none',
+  justifyContent: 'flex-end',
+  zIndex: 1,
+  boxShadow: '$500',
+  height: '100%',
+
+  '@md': {
+    display: 'flex',
+    position: 'relative',
+    boxShadow: 'revert',
+  },
+
+  variants: {
+    isOpen: {
+      true: {
+        display: 'flex',
+      },
+    },
+  },
+});
+
+export const ShowcaseWrapper = styled('div', {
+  flex: 3,
+  padding: '32px',
+  background: '$bg',
+  height: '100%',
+  overflowY: 'auto',
+
+  '@sm': {
+    padding: '32px 48px 48px',
+  },
+
+  '@lg': {
+    padding: '32px 64px 64px',
+  },
+});
+
+export const ShowcaseContent = styled('div', {
+  '@md': {
+    maxWidth: 600,
+  },
+});
+
+export const SidebarOverlay = styled('div', {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  backgroundColor: 'rgba(60, 63, 75, 0.65)',
+  backdropFilter: 'blur(4px)',
+  zIndex: 1,
+
+  '@md': {
+    display: 'none',
+  },
+});
